4065e21aaed6e461c44b681feefb02d222d3a537
JosephSamuel2022 <josephsamuelm2021@gmail.com>
2023-08-16 19:25:33 +0530

    front end deployment

--- Back-End/index.js
+++ Back-End/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: "https://groupchatreact.vercel.app/",
+		origin: "https://groupchatreact.vercel.app",
 		methods: ["GET", "POST"],
 	},
 });
